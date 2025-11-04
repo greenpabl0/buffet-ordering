d7ab37451e150150fb5017ca6700646382480e40
Sirisak-Jalkueng <sirisakjk@gmail.com>
2025-11-04 21:20:24 +0700

    Update_db
    - Remove table customer
    - Add attributes number of adults and children with price  at table orders
    - Add attributes buffet amount and other amount at table bill

--- Buffet-MySQL.sql
+++ Buffet-MySQL.sql
@@ -2,22 +2,13 @@ drop schema if exists Buffet;
 create schema Buffet;
 use Buffet;
 
-drop table customer;
 drop table bill;
 drop table employee;
-drop table employee_role;
 drop table menu;
 drop table orders;
 drop table order_detail;
 drop table restaurant_table;
 
-create table customer 
-	(
-    customer_id 	int auto_increment primary key,
-    name         	varchar(100) not null,
-    phone        	varchar(20),
-    member_type  	varchar(20)
-	);
 
 create table restaurant_table 
 	(
@@ -40,20 +31,28 @@ create table menu
     menu_name 		varchar(100) not null,
     category 		varchar(50),
     price 			decimal(10,2) not null check (price >= 0),
-    is_buffet 		boolean default false
+    is_buffet 		boolean default true
 	);
 
 create table orders 
 	(
     order_id 		int auto_increment primary key,
     table_id 		int not null,
+    emp_id          int,
     start_time 		datetime not null,
     end_time 		datetime,
     status 			enum('Open','Closed','Cancelled') default 'Open',
+
+    num_adults      int not null check (num_adults >= 0),
+    num_children    int not null check (num_children >= 0),
+    adult_price     decimal(10,2) not null,
+    child_price     decimal(10,2) not null,
     num_of_customers int check (num_of_customers > 0),
     foreign key (table_id) references restaurant_table(table_id)
         on update cascade
         on delete restrict
+    foreign key (emp_id) references employee(emp_id)
+        on delete set null
 	);
 
 create table order_detail 
@@ -74,16 +73,15 @@ create table bill
 	(
     bill_id 		int auto_increment primary key,
     order_id 		int not null,
-    customer_id 	int,
-    emp_id 	int,
+    emp_id 	        int,
+    buffet_amount   decimal(10,2),
+    other_amount    decimal(10,2),
     total_amount 	decimal(10,2) not null,
     discount 		decimal(10,2) default 0,
     net_amount 		decimal(10,2) generated always as (total_amount - discount) stored,
     pay_time 		datetime default current_timestamp,
     foreign key (order_id) references orders(order_id)
         on delete cascade,
-    foreign key (customer_id) references customer(customer_id)
-        on delete set null,
     foreign key (emp_id) references employee(emp_id)
         on delete set null
 	);
